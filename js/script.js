ab96dff1e230817e0d31e46a6e57cd24634ef8d9
Eric Zhu <ericz2241@gmail.com>
2016-04-09 20:01:34 -0400

    Implemented and customized CSS overlay from Kickdrop.
    Kickdrop's overlay package is seriously awesome. Check it out here:
    http://kickdropshop.com/drops/expanding-content-overlay/

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
+(function() {
+  $(function() {
+    return Boxgrid.init();
+  });
+
+}).call(this);
 
 $('body').on('click', '.js-expand-link', function (event) {
     
