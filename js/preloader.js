4533b56af3e9a514bbde047f05ea5e405953085c
Eric Zhu <ericz2241@gmail.com>
2016-04-20 21:39:50 -0400

    Revert "Don't look at this."
    This reverts commit d6e54e28ed970c2311d2bdc8fd544efecb4d453f.

--- js/preloader.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {  
-
-// site preloader -- also uncomment the div in the header and the css style for #preloader
-$(window).load(function(){
-	$('.preloader').fadeOut('slow',function(){$(this).remove();});
-});
-
-});
