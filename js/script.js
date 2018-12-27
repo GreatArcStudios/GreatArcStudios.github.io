7027c12e31a9c4ab49db339a67cab768fbece16b
Eric Zhu <ericz2241@gmail.com>
2018-12-26 23:04:02 -0500

    Finished redesign

--- js/script.js
+++ js/script.js
@@ -1,16 +1,130 @@
-$(function () {
-    $(".middle-label")
+var sections = [$("#code"), $("#design"), $("#about"), $(".overlay-top")];
+
+$(document).ready(function () {
+    $("#about").click(function () {
+        $("#about-box").css("z-index", 20);
+        $("#about-box").animate({
+            opacity: "1",
+            height: "100vh",
+            width: "100vw"
+        }, 850);
+    });
+
+    $("#close-about").click(function () {
+        $("#about-box").animate({
+            opacity: "0",
+            height: "100vh",
+            width: "50vw",
+            left: "25%"
+        }, 850);
+        $("#about-box").promise().done(
+            function () {
+                $("#about-box").css("z-index", -10);
+                for (let i = 0; i < sections.length; i++) {
+                    if (i == 2) {
+                        sections[i].removeClass("full-expand");
+                    } else {
+                        sections[i].removeClass("full-minimize");
+                    }
+                }
+                $("#about-box").css("left", "");
+            });
+
+    });
+    $("#code").click(function () {
+        $("#code-box").css("z-index", 20);
+        $("#code-box").animate({
+            opacity: "1",
+            height: "100vh",
+            width: "100vw"
+        }, 850);
+        $("#code-box").promise().done(
+            function () {
+                $("#close-code").attr("style", "position:fixed");
+                sections[0].css("position", "fixed");
+                $("#code-box").css("overflow", "visible");
+            }
+        );
+    });
+
+    $("#close-code").click(function () {
+        $("#code-box").animate({
+            opacity: "0",
+            height: "100vh",
+            width: "50vw",
+            left: "25%"
+        }, 850);
+        $("#code-box").promise().done(
+            function () {
+                $("#code-box").css("z-index", -10);
+                for (let i = 0; i < sections.length; i++) {
+                    if (i == 0) {
+                        sections[i].removeClass("full-expand");
+                        sections[0].css("position", "");
+                    } else {
+                        sections[i].removeClass("full-minimize");
+                    }
+                }
+                $("#close-code").attr("style", "position:");
+                $("#code-box").css("left", "");
+                $("#code-box").css("overflow", "hidden");
+            });
+
+    });
+
+    $("#design").click(function () {
+        $("#design-box").css("z-index", 20);
+        $("#design-box").animate({
+            opacity: "1",
+            height: "100vh",
+            width: "100vw"
+        }, 850);
+        $("#design-box").promise().done(
+            function () {
+                $("#close-design").attr("style", "position:fixed");
+                sections[1].css("position", "fixed");
+                $("#design-box").css("overflow", "visible");
+            }
+        );
+    });
+
+    $("#close-design").click(function () {
+        $("#design-box").animate({
+            opacity: "0",
+            height: "100vh",
+            width: "50vw",
+            left: "25%"
+        }, 850);
+        $("#design-box").promise().done(
+            function () {
+                $("#design-box").css("z-index", -10);
+                for (let i = 0; i < sections.length; i++) {
+                    if (i == 1) {
+                        sections[i].removeClass("full-expand");
+                        sections[1].css("position", "");
+                    } else {
+                        sections[i].removeClass("full-minimize");
+                    }
+                }
+                $("#close-design").attr("style", "position:");
+                $("#design-box").css("left", "");
+                $("#design-box").css("overflow", "hidden");
+            });
+
+    });
+
 });
 
 
 code = document.querySelector("#code");
 design = document.querySelector("#design");
 about = document.querySelector("#about");
-frontElements = [code, design, about];
+overlayTop = document.querySelector(".overlay-top");
+frontElements = [code, design, about, overlayTop];
 
 
 code.addEventListener("mouseenter", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (let i = 2; i >= 0; i--) {
         if (Object.is(frontElements[i], code)) {
             frontElements[i].classList.add("expand");
         } else {
@@ -20,7 +134,7 @@ code.addEventListener("mouseenter", () => {
 
 });
 code.addEventListener("mouseleave", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (i = 0; i < frontElements.length - 1; i++) {
         if (Object.is(frontElements[i], code)) {
             frontElements[i].classList.remove("expand");
         } else {
@@ -30,7 +144,7 @@ code.addEventListener("mouseleave", () => {
 });
 
 code.addEventListener("click", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (let i = 0; i < frontElements.length; i++) {
         if (Object.is(frontElements[i], code)) {
             frontElements[i].classList.remove("expand");
             frontElements[i].classList.add("full-expand");
@@ -42,7 +156,7 @@ code.addEventListener("click", () => {
 });
 
 design.addEventListener("mouseenter", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (let i = 0; i < frontElements.length - 1; i++) {
         if (Object.is(frontElements[i], design)) {
             frontElements[i].classList.add("expand");
         } else {
@@ -52,7 +166,7 @@ design.addEventListener("mouseenter", () => {
 });
 
 design.addEventListener("mouseleave", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (i = 0; i < frontElements.length - 1; i++) {
         if (Object.is(frontElements[i], design)) {
             frontElements[i].classList.remove("expand");
         } else {
@@ -62,10 +176,11 @@ design.addEventListener("mouseleave", () => {
 });
 
 design.addEventListener("click", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (let i = 0; i < frontElements.length; i++) {
         if (Object.is(frontElements[i], design)) {
-            frontElements[i].classList.remove("expand");
             frontElements[i].classList.add("full-expand");
+            frontElements[i].classList.remove("expand");
+
         } else {
             frontElements[i].classList.remove("minimize");
             frontElements[i].classList.add("full-minimize");
@@ -74,7 +189,7 @@ design.addEventListener("click", () => {
 });
 
 about.addEventListener("mouseenter", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (i = 0; i < frontElements.length - 1; i++) {
         if (Object.is(frontElements[i], about)) {
             frontElements[i].classList.add("expand");
         } else {
@@ -84,7 +199,7 @@ about.addEventListener("mouseenter", () => {
 });
 
 about.addEventListener("mouseleave", () => {
-    for (i = 2; i >= 0; i--) {
+    for (let i = 0; i < frontElements.length - 1; i++) {
         if (Object.is(frontElements[i], about)) {
             frontElements[i].classList.remove("expand");
         } else {
@@ -94,7 +209,7 @@ about.addEventListener("mouseleave", () => {
 });
 
 about.addEventListener("click", () => {
-    for (i = 0; i < frontElements.length; i++) {
+    for (let i = 0; i < frontElements.length; i++) {
         if (Object.is(frontElements[i], about)) {
             frontElements[i].classList.remove("expand");
             frontElements[i].classList.add("full-expand");
